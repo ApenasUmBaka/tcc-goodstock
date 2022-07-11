ee2d9e80ef1a52af5b75772878fe5f3ddd6592a9
Saiki <GSaiki26@gmail.com>
2022-07-11 19:51:07 -0300

    Added: Api-Customer - Logger
    Added a logger object

--- api/customers/src/index.ts
+++ api/customers/src/index.ts
@@ -2,14 +2,17 @@
 import Helmet from "helmet";
 import Express from "express";
 
+import LoggerFactory from "@logger";
+
 // Data
 const PORT = process.env.PORT;
 const app = Express();
+const logger = LoggerFactory.createLogger('Server');
 
 // Code
 app.use(Helmet());
 app.set("trust proxy", true);
 
 app.listen(PORT, () => {
-  console.log(`The server is online on port: ${PORT}`);
+  logger.info(`The server is online on port: ${PORT}`);
 });
