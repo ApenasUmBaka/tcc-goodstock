ee33f07af1442fba34fcb2f27ecc5678417e3e04
ApenasUmBaka <andresss2004@gmail.com>
2022-08-10 20:53:15 -0300

    Uptade Items e Visual
    Delete animation typescript, New Palette of Colors, Reveal Animation, Add new Slide Show, Selection text is pink.

--- back-end/src/views/assets/scripts/loading.js
+++ back-end/src/views/assets/scripts/loading.js
@@ -1,4 +1,18 @@
-jQuery(window).load(function () {
-    $(".loader").delay(5000).fadeOut("slow"); 
-  $("#tudo_page").toggle("fast");
-});
+const fadeOut = () => {
+    const loaderWrapper =
+    document.querySelector('.wrapper');
+    loaderWrapper.classList.add('fade');
+}
+
+window.addEventListener('load', fadeOut);
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 5000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
