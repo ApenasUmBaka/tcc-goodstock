dce72db0112758280ad9f30d797d1804d8199e0d
Saiki <GSaiki26@gmail.com>
2022-11-22 05:31:04 +0000

    Added: Products Div

---
+++ backend/src/views/assets/scripts/workspace/eventsDefinition.ts
@@ -0,0 +1,21 @@
+// Functions
+/**
+ * A function to remove the current visible section.
+ */
+function toggleVisibleSection(nextSection: string) {
+  // Remove the visible section.
+  return () => {
+    const oldSection = document.getElementsByClassName('section-visible')[0];
+    oldSection.classList.remove('section-visible');
+    oldSection.setAttribute('style', 'display:none');
+
+    // Show the wished section.
+    const section = document.getElementById(nextSection)!;
+    section.classList.add('section-visible');
+    section.removeAttribute('style');
+  }
+}
+
+// Code
+document.getElementById('a-sidebar-dashboard')!.onclick = toggleVisibleSection('section-dashboard');
+document.getElementById('a-sidebar-stock')!.onclick = toggleVisibleSection('section-stock');
