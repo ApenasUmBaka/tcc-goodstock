16f47a97252913ad0330fabefb82a0a58370971a
André Sousa Soares Da Silva <86202392+ApenasUmBaka@users.noreply.github.com>
2022-12-07 00:17:24 -0300

    Feature/frontend homepage (#52)
    * Homepage and cadlogin styles
    
    Footer homepage, cadlogin modal organization
    
    * Fix merged Develop
    
    * Test Grid Homapage
    
    * Main News fix bug
    
    Fix responsive video, edit footer, new style button style.
    
    * Upadate Homepage Styles
    
    Texts of GoodStock section, card news update
    
    * Update home-page-style.css
    
    * Update work-station-style.css

--- backend/src/views/assets/scripts/progressBar.js
+++ backend/src/views/assets/scripts/progressBar.js
@@ -1,17 +1,15 @@
 window.addEventListener("scroll", () => {
-  // Calcul de la hauteur "utile" du document
 
-  let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+  let height = document.documentElement.scrollHeight - window.innerHeight;
 
-  // Récupération de la position verticale
   let position = window.scrollY;
 
-  // Récupération de la largeur de la fenêtre
-  let largeur = document.documentElement.clientWidth;
 
-  // Calcul de la largeur de la barre
-  let barre = (position / hauteur) * largeur;
+  let width = document.documentElement.clientWidth;
 
-  // Modification du CSS de la barre
-  document.getElementById("progress").style.width = barre + "px";
+ 
+  let bar = (position / height) * width;
+
+
+  document.getElementById("progress").style.width = bar + "px";
 });
